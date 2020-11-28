1c73fd3b5910de8c233e43355be43c7198a28bff
John Eagle <>
2020-11-27 21:09:51 -0500

    button test

--- button_test1.py
+++ button_test1.py
@@ -2,13 +2,13 @@ import RPi.GPIO as GPIO
 import time
 
 ledPin = 18
-buttonPin = 16
+button_pin = 16
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while(true):
-    if GPIO.input(buttonPin):
+    if GPIO.input(button_pin):
         print("BUTTON PRESS")
     time.sleep(0.25)
